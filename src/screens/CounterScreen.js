58c278e3fd2ea5d5170c0bacab18574aa61725e1
TanimaRanjan <tanimaranjan@gmail.com>
2020-06-13 14:59:50 -0400

    Change Counter to use Reducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,19 +1,31 @@
-import React , {useState}  from 'react'
+import React , {useReducer}  from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
+const reducer = (state, action) => {
+
+    switch(action.type) {
+        case 'INC_COUNT':
+            return {...state, count:state.count+action.payload}
+        case 'DEC_COUNT':
+            return {...state, count:state.count-action.payload}
+        default: return state
+    }
+}
 
 const CounterScreen = () => {
 
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
+    const [{count}, dispatch] = useReducer(reducer, {count : 0})
     return (
         <View style={style.viewStyle}>
+            <Text style={style.textStyle}>Change Count</Text>
             <Button 
-                
-                onPress={() => setCount(count+1)}
+
+                onPress={() => dispatch({type:'INC_COUNT', payload:1})}
                 title="INCREASE"></Button>
             <Button 
                
-                onPress={() => setCount(count-1)}
+                onPress={() => dispatch({type:'DEC_COUNT', payload:1})}
                 title="DECREASE"></Button>
             <Text style={style.textStyle}>Current Count:  {count}</Text>
         </View>
@@ -22,16 +34,16 @@ const CounterScreen = () => {
 
 const style = StyleSheet.create({
     textStyle: {
-        fontSize: 25
-        // backgroundColor:'lightblue',
-        // marginTop:50,
-        // borderBottomWidth:67, 
+        fontSize: 25,
+        textAlign:"center",
+        margin:'10%'
        
     }, 
     viewStyle: {
         display:"flex",
         justifyContent:"center",
-        alignContent:"center"
+        alignContent:"center",
+        marginTop:'10%'
     }
 })
 
